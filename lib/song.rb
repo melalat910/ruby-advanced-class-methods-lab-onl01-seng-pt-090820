57f0b4b8da5e7d4f482adc32555b49456f98593a
Tirzah P. Howard <tp.howard47@gmail.com>
2020-10-11 04:05:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,53 +1,69 @@
-class Artist
-  attr_accessor :name, :song
+class Song
+  attr_accessor :name, :artist_name
   @@all = []
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def self.name=
-    @name
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def songs
-    @songs
-  end
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
-  def self.new_by_name(name)
-    artist_name = Song.new(name)
-    artist_name
-  end
-
-  def self.create_by_name(name = nil)
-    artist = self.new_by_name(name)
-    artist.save
-    artist
-  end
-
-  def self.find_by_name(name = nil)
-    @@all.detect{|x| x.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    if find_by_name(name) == nil
-      create_by_name(name)
-    else
-      find_by_name(name)
-    end
-  end
-
-end
+ 
+  def self.create
+     song = self.new
+     song.save
+     song
+   end 
+ 
+   def self.new_by_name(name)
+     song = self.new
+     song.name = name
+     song
+   end 
+ 
+   def self.create_by_name(name)
+     song = self.create
+     song.name = name 
+     song
+   end 
+ 
+   def self.find_by_name(name)
+     @@all.each do |song|
+       if song.name == name
+         return song
+       end 
+     end
+   end 
+ 
+   def self.find_or_create_by_name(name)
+     self.find_by_name(name) || self.create_by_name(name)
+   end
+ 
+ 
+   def self.alphabetical
+ 
+     @@all.sort_by{|song| song.name}
+ 
+   end
+ 
+   def self.new_from_filename(filename)
+ 
+     row = filename
+ 
+       data = row.split(" - ")
+       artist_name = data[0]
+       song_name = data[1].gsub(".mp3", "")
+ 
+     song = self.new
+     song.name = song_name
+     song.artist_name = artist_name
+     song
+ end
+ 
+   def self.create_from_filename(filename)
+     song = self.new_from_filename(filename)
+     song.save
+     song
+   end
+ end
